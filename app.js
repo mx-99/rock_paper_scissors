85e281466c3633b23cfff36e4a9e2007f1f00526
mx-99 <125824465+mx-99@users.noreply.github.com>
2024-01-13 20:08:01 +0300

    modified app.js and index.html

--- app.js
+++ app.js
@@ -1,82 +1,78 @@
-function computerChoice(){
-  const rps = ['rock', 'paper', 'scissor'];
-  const rps_index = Math.floor(Math.random() * rps.length);
-  const rps_random = rps[rps_index];
-  return rps_random;
-}
+let body = document.querySelector('body')
+let buttons = document.createElement('div')
+let div_score = document.createElement('div')
+let p_vs_c = document.createElement('p')
+let computer_score = document.createElement('p')
+let player_score = document.createElement('p')
+let rock = document.createElement('button')
+let paper = document.createElement('button')
+let scissor = document.createElement('button')
+player_score.textContent = 'You  = ';
+computer_score.textContent = 'Computer = ';
+//button names
+rock.textContent = 'rock'
+paper.textContent = 'paper'
+scissor.textContent = 'scissor'
 
-function playerChoice(){
-  let userInput = prompt("Enter Rock, Paper or Scissor to play agianst a computer");
-  return userInput.toLowerCase().trim();
-}
+// Event Listeners
+rock.addEventListener('click', function () {
+  playRound('rock')
+})
+paper.addEventListener('click', function () {
+  playRound('paper')
+})
+scissor.addEventListener('click', function () {
+  playRound('scissor')
+})
 
-function play() {
-    //Rock beats Scissors:
-  //Scissors beat Paper
-  //Paper beats Rock
-    let player_score = 0;
-  let computer_score = 0;
-  let player_wins = 0;
-  for(i=0; i<5; i++)
-  {
+  //childs append
+body.appendChild(buttons);
+body.appendChild(div_score);
+body.appendChild(p_vs_c);
+div_score.appendChild(player_score);
+div_score.appendChild(computer_score);
+buttons.appendChild(rock);
+buttons.appendChild(paper);
+buttons.appendChild(scissor);
 
-    let player = playerChoice();
-    let computer = computerChoice();
-    if(player_score === 3 || computer_score === 3)
-    {  
-       if (player_score > computer_score)
-      {
-        console.log("So Human won ", player_score);
-      }
-      else{
-        console.log("So Computer won ", computer_score);
-        }
-      break;
-    }
-    console.log("Starting round " + (i+1));
+let result = '';
+let p_score = 0;
+let c_score = 0;
+//play
+function playRound(playerChoice) { 
+  const choices = ['rock', 'paper', 'scissor'];
+  const choices_index = Math.floor(Math.random() * choices.length);
+  const computerChoice = choices[choices_index];
 
-     if(player === computer){
-      console.log("it's a tie")
-    }
-    else if(player == 'rock' && computer == 'scissor'){
-      player_score +=1;
-      console.log("you win Rock beats Scissor")
-    }
-    else if(player == 'scissor' && computer == 'paper'){
-      player_score +=1;
-      console.log("you win Scissor beats paper")
-    }
-    else if(player == 'paper' && computer == 'rock'){
-       player_score +=1;
-       console.log("you win Rock beats Scissor")
-    }
-    else if(computer == 'rock' && player == 'scissor'){
-      computer_score +=1;
-      console.log("you lost Rock beats Scissor")
-    }
-    else if(computer == 'scissor' && player == 'paper'){
-      computer_score +=1;
-      console.log("you lost Scissor beats paper")
-    }
-    else if(computer == 'paper' && player == 'rock'){
-       computer_score +=1;
-       console.log("you lost Rock beats Scissor")
-    }
-    else {
-      console.log("invalid input");
-    }
-   console.log("Ending round " + (i+1));
+  if (playerChoice == computerChoice) {
+    p_vs_c.innerText = `${computerChoice.toUpperCase()} VS ${playerChoice.toUpperCase()} = Tie`;
+  } else if (
+    (playerChoice == 'rock' && computerChoice == 'scissor') ||
+    (playerChoice == 'paper' && computerChoice == 'rock') ||
+    (playerChoice == 'scissor' && computerChoice == 'paper')
+  ) {
+    p_score += 1
+    p_vs_c.innerText = `You Win! ${playerChoice.toUpperCase()} Beats ${computerChoice.toUpperCase()} `
+    player_score.innerText = `You = ${p_score}`
+  } else {
+    c_score += 1
+    p_vs_c.innerText = `You lost!!! ${computerChoice.toUpperCase()} Beats ${playerChoice.toUpperCase()} `
+    computer_score.innerText = `Computer = ${c_score}`
   }
-  
-console.log("The score is " + player_score + " For the Human And "  + computer_score + " For the computer");
-  if (player_score === computer_score){
-    console.log("So The match is a draw ", player_score, computer_score);
-  }
-  else if (player_score > computer_score){
-    console.log("So Human won ", player_score);
-  }
-  else{
-    console.log("So Computer won ", computer_score);
+
+  if (p_score >= 5) {
+    setTimeout(() => {
+      alert(result.textContent = `You win::  You ${p_score} Computer ${c_score} `);
+      location.reload();
+    }, 0)
+    return
+  } else if (c_score >= 5) {
+      setTimeout(() => {
+      alert(result.textContent = `You lost::  Computer ${c_score} You ${p_score} `);
+      location.reload();    
+    }, 0)
+    return
   }
 }
-play()
+
+
