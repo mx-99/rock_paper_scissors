cecafb7be55e308cd7c0da20455dabdf1e73da65
mx-99 <125824465+mx-99@users.noreply.github.com>
2024-05-11 17:22:45 +0000

    Add getHumanChoie function

--- app.js
+++ app.js
@@ -3,4 +3,12 @@ let choices = ['rock', 'paper', 'scissor'];
 randomIndex = Math.floor(Math.random()* choices.length);
 return choices[randomIndex];
 }
-console.log(getComputerChoice());
+
+function getHumanChoice(){
+    let humanInput = prompt("Choose rock, paper or scissor");
+
+    return humanInput;
+}
+
+
+
